713b8476be3af8409481adada8d625b3d837597d
Rifqi Aria <rifqiaria95@outlook.com>
2026-04-05 23:32:50 +0700

    fix: api.php 2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,7 +7,6 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
@@ -16,11 +15,6 @@ return Application::configure(basePath: dirname(__DIR__))
     ])
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->trustProxies(at: '*');
-        
-        // Cara 2: Register hanya untuk API routes
-        $middleware->group('api', [
-            \App\Http\Middleware\Cors::class,
-        ]);
 
         $middleware->alias([
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
